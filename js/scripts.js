8bd193331e328541aa65f22be0716bc7c24d332e
achmadjp <achmadjeihan@gmail.com>
2015-12-08 07:39:44 +0700

    adding drop zone to html

--- js/scripts.js
+++ js/scripts.js
@@ -138,7 +138,7 @@
       previewsContainer: null,
       hiddenInputContainer: "body",
       capture: null,
-      dictDefaultMessage: "Drop files here to upload",
+      dictDefaultMessage: "Drag and drop file here",
       dictFallbackMessage: "Your browser does not support drag'n'drop file uploads.",
       dictFallbackText: "Please use the fallback form below to upload your files like in the olden days.",
       dictFileTooBig: "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.",
